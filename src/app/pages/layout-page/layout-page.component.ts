701ecd95c9eb5611c69b27d98d658aa8a95ddd87
EderGodinez <eder.godinez9511@alumnos.udg.mx>
2023-11-16 16:23:46 -0600

    feat:Se realizo layout de aplicacion

--- src/app/pages/layout-page/layout-page.component.ts
+++ src/app/pages/layout-page/layout-page.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   templateUrl: './layout-page.component.html',
   styleUrls: ['./layout-page.component.scss']
 })
 export class LayoutPageComponent {
-
+  items: MenuItem[] | undefined;
+  activeItem: MenuItem | undefined;
+  ngOnInit() {
+      this.items = [
+          { label: 'Inicio', icon: 'pi pi-fw pi-home',routerLink:'Home' },
+          { label: 'Mapa', icon: 'pi pi-map-marker',routerLink:'Map' },
+          { label: 'Reservaciones', icon: 'pi pi-fw pi-calendar',routerLink:'Reservations' },
+          { label: 'Iniciar sesion', icon: 'pi pi-user',routerLink:'Login' },
+          { label: 'Crear cuenta', icon: 'pi pi-fw pi-cog',routerLink:'Register' }
+      ];
+      this.activeItem = this.items[0];
+  }
+  onActiveItemChange(event: MenuItem) {
+      this.activeItem = event;
+  }
 }
